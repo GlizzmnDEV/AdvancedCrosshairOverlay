cb3edd289d06a6cd4193b929852a9868365761ca
GlizzmnDEV <saggermn1@gmail.com>
2025-07-15 11:11:39 -0500

    PluginInfo.cs

---
+++ CrosshairOverlay/PluginInfo.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CrosshairOverlay
+{
+
+    public static class PluginInfo
+    {
+
+        public const string PLUGIN_GUID = "AdvancedCrosshairOverlay";
+
+
+        public const string PLUGIN_NAME = "AdvancedCrosshairOverlay";
+
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
